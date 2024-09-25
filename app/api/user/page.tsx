b42cbb99866f207b0f4f5f65f196901eee3ef937
jainam-b <jainambagrecha16@gmail.com>
2024-09-25 22:46:44 +0530

    updates

--- app/api/user/page.tsx
+++ app/api/user/page.tsx
@@ -1,19 +1,20 @@
 // components/Profile.js
 "use client"
-import { RootState } from '@/app/lib/store';
-import { useSelector } from 'react-redux';
+// import { RootState } from '@/app/lib/store';
+// import { useSelector } from 'react-redux';
 
-const  page= () => {
-  const { isLoggedIn, user } = useSelector((state:RootState) => state.auth);
+const page = () => {
+  // const { isLoggedIn, user } = useSelector((state: RootState) => state.auth);
 
   return (
     <div>
-      {isLoggedIn ? (
-        <h1>Welcome, {user.name}</h1>
-        
+      {/* {isLoggedIn ? (
+        // TypeScript error here because 'username' does not exist on 'user'
+        <h1>Welcome, {user.username}</h1> 
       ) : (
         <h1>Please log in</h1>
-      )}
+      )} */}
+      <h1>Please log in</h1> 
     </div>
   );
 };
